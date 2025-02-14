9bc47658278109fc5202ea7e0fdcd0416e334eb2
romarioburke <romari123456789@gmail.com>
2025-02-14 16:22:58 -0500

    forgot password initial page  layout - not completed

--- src/pages/Authentication/ForgotPasswordPage.tsx
+++ src/pages/Authentication/ForgotPasswordPage.tsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { MdLockReset } from "react-icons/md";
 const ForgetPasswordPage = ({ onSwitch }: { onSwitch: (view: "signin" | "signup" | "forgotpassword") => void }) => {
-    return (
-      <div>
-        <h2 className="text-2xl font-bold text-center mb-4">Sign In</h2>
-        <form>
-          <input type="email" placeholder="Email" className="w-full p-2 mb-2 border rounded" />
-          <input type="password" placeholder="Password" className="w-full p-2 mb-4 border rounded" />
-          <button className="w-full bg-blue-500 text-white p-2 rounded">Login</button>
-        </form>
-        <div className="mt-4 text-center">
-          <button className="text-sm text-blue-500" onClick={() => onSwitch("forgotpassword")}>
-            Forgot Password?
-          </button>
-          <p className="text-sm mt-2">
-            Don't have an account?{" "}
-            <button className="text-blue-500" onClick={() => onSwitch("signup")}>
-              Sign Up
-            </button>
-          </p>
+  const [email, setEmail] = useState("");
+  return (
+    <>
+      <div className="p-6 w-full h-full bg-gray-800">
+        <div>
+          <ul className="steps mx-auto flex justify-center text-white">
+            <li className="step step-primary before:white mr-4">Register</li>
+            <li className="step">Verify</li>
+            <li className="step ml-5">Update Password</li>
+          </ul>
+          <form  className="flex flex-col items-center mt-4">
+          <MdLockReset size={200} className="text-indigo-500"/>
+          <h2 className="text-2xl font-bold text-center mb-4">Forget Password</h2>
+          <span className="md:text-2x1 mb-2">Please provide your account's email!</span>
+            <label className="input input-bordered flex items-center w-full gap-2 mx-6">
+              <MdOutlineEmail className="h-4 w-4 opacity-70" />
+              <input type="text" className="grow" placeholder="Please enter your name"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <button className="bg-indigo-500 text-white p-2 rounded mt-5 w-2/3 mx-auto">Reset Password</button>
+          </form>
+          <div className="mt-4 text-center">
+            <p className="text-sm mt-2">
+              Don't have an account?
+              <button className="ml-1 text-indigo-500" onClick={() => onSwitch("signup")}>
+                Sign Up
+              </button>
+            </p>
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default ForgetPasswordPage;
+    </>
+
+  );
+};
+
+export default ForgetPasswordPage;
