ba4b51a9eabfb73395f5ab21b2caa88a9cd4954a
romarioburke <romari123456789@gmail.com>
2025-02-17 23:48:26 -0500

    update the forget password steps component

--- src/pages/Authentication/ForgotPasswordPage.tsx
+++ src/pages/Authentication/ForgotPasswordPage.tsx
@@ -3,14 +3,20 @@ import { MdOutlineEmail } from "react-icons/md";
 import { MdLockReset } from "react-icons/md";
 const ForgetPasswordPage = ({ onSwitch }: { onSwitch: (view: "signin" | "signup" | "forgotpassword") => void }) => {
   const [email, setEmail] = useState("");
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const handleInitial = async (e: React.FormEvent) => {
+      e.preventDefault();
+    setCurrentStep(currentStep + 1);
+  }
   return (
     <>
       <div className="p-6 w-full h-full bg-gray-800">
         <div>
-          <ul className="steps mx-auto flex justify-center text-white">
-            <li className="step step-primary before:white mr-4">Register</li>
-            <li className="step">Verify</li>
-            <li className="step ml-5">Update Password</li>
+        <ul className="steps  w-full grid grid-cols-3 md:w-3/4 md:mx-auto text-white [&_li.step:before]:text-white">
+          <li className={`step ${currentStep >= 1 ? "step-primary" : ""}`}>Register</li>
+          <li className={`step ${currentStep >= 2 ? "step-primary" : ""}`}>Verify</li>
+          <li className={`step ${currentStep >= 3 ? "step-primary" : ""}`}>Update Password</li>
           </ul>
           <form  className="flex flex-col items-center mt-4">
           <MdLockReset size={200} className="text-indigo-500"/>
@@ -22,7 +28,7 @@ const ForgetPasswordPage = ({ onSwitch }: { onSwitch: (view: "signin" | "signup"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </label>
-            <button className="bg-indigo-500 text-white p-2 rounded mt-5 w-2/3 mx-auto">Reset Password</button>
+            <button className="bg-indigo-500 text-white p-2 rounded mt-5 w-2/3 mx-auto" onClick={(E)=>handleInitial(E)}>Reset Password</button>
           </form>
           <div className="mt-4 text-center">
             <p className="text-sm mt-2">
